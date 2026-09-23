81916ddccf5b10e32d5bad2863e77fe81407ff34
Song <lucifarchy@gmail.com>
2026-09-23 21:26:28 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,5 +6,4 @@
 // ============================================================
 
 export const SUPABASE_URL = 'https://mpxaqrawlwbvxpbdypyc.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1weGFxcmF3bHdidnhwYmR5cHljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAxNDU3MzMsImV4cCI6MjEwNTcyMTczM30.SR5h6ndFHB_9xyh_vqrCL3p3c_4Ez-ydVQg3SClTZv4
-';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1weGFxcmF3bHdidnhwYmR5cHljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAxNDU3MzMsImV4cCI6MjEwNTcyMTczM30.SR5h6ndFHB_9xyh_vqrCL3p3c_4Ez-ydVQg3SClTZv4';
